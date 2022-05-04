ef52bfecee4dd2e6baff84b7375241c12af67d3d
ayuanlmo <ayuan@ayuanlmo.cn>
2022-05-05 00:21:37 +0800

    添加Nprogress

---
+++ src/lib/Nprogress/index.t.js
@@ -0,0 +1,22 @@
+/**
+ * Nprogress
+ * @author ayuanlmo
+ * @module nprogress
+ * Created by ayuanlmo on 2022/04
+ * **/
+
+import Nprogress from 'nprogress';
+require('./style.t.scss');
+
+export const start = (d = 0.1) => {
+    Nprogress.set(d);
+    setTimeout(async () => {
+        await Nprogress.start();
+    });
+};
+
+export const done = () => {
+    setTimeout(async () => {
+        await Nprogress.done();
+    });
+};
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import routes from './routes';
+import * as Nprogress from '@/lib/Nprogress/index.t';
+
+const routerConf = require('@/config/AppConfig').router;
 
 Vue.use(VueRouter);
 
-const router = new VueRouter({
+const RouterCore = new VueRouter({
     ...{
-        mode: 'history',
-        base: process.env.BASE_URL,
-        routes
+        mode: routerConf.mode,
+        base: routerConf.base,
+        routes: [].concat(...routes)
     }
 });
 
-export default router;
+RouterCore.beforeEach(async (to, from, next) => {
+    Nprogress.start();
+    await next();
+});
+
+RouterCore.afterEach(async () => {
+    await Nprogress.done();
+});
+
+export default RouterCore;
