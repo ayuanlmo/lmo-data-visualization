bcf351a7d34db98715a004e80539acf850aa77bf
ayuanlmo <ayuan@ayuanlmo.cn>
2022-05-05 23:25:00 +0800

    添加动画

--- src/components/TemplateItem/index.t.js
+++ src/components/TemplateItem/index.t.js
@@ -45,7 +45,6 @@ export default {
                                     class: ''
                                 }, [this.data.description])
                             ])
-
                         ])
                     ])
                 ])
