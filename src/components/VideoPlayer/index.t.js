e28526214947150497e4373535a341e92272caad
ayuanlmo <ayuan@ayuanlmo.cn>
2022-08-19 00:03:55 +0800

    feat：调整视频播放器

--- src/components/VideoPlayer/index.t.js
+++ src/components/VideoPlayer/index.t.js
@@ -1,50 +1,24 @@
 export default {
     name: 'lmo-video_player',
     render(h) {
-        return (
-            <el-dialog before-close={() => {
-                this.visible = false;
-            }} width={'34vw'} style={'margin-top: 15vh;z-index: 10;background-color:#ffffff00;'}
-                       title={'lmo VideoPlayer'} visible={this.visible}>
-                <video muted ref={'video'} onCanPlay={() => {
-                    if (this.visible)
-                        this.$refs.video['play']();
-                    this.videoConf.duration = parseInt(this.$refs.video['duration']);
-                }} onTimeUpdate={() => {
-                    this.videoConf.currentTime = this.$refs.video['currentTime'];
-                    this.videoConf.percent = parseInt((this.videoConf.currentTime / this.videoConf.duration * 100).toFixed(2)) + 1;
-                    this.sliderValue = this.videoConf.percent;
-                }} style={'width: 32vw;height: 18vw;'}
-                       src={'/server' + this.path}/>
-                <div>
-                    {
-                        h('el-slider', {
-                            props: {
-                                disabled: true,
-                                value: this.sliderValue,
-                                max: 100
-                            }
-                        })
-                    }
-                </div>
-            </el-dialog>
-        );
+        return <el-dialog before-close={() => {
+            this.visible = false;
+        }} width={'50vw'} style={'z-index: 10;background-color:#ffffff00;'}
+                          title={'lmo VideoPlayer'} visible={this.visible}>
+            <video muted controls ref={'video'} onCanPlay={() => {
+                if (this.visible) this.$refs.video['play']();
+            }} style={'width: 48vw;height: 27vw;border-radius: 0.5rem;'}
+                   src={`${require('@/config/AppConfig').devProxy.http}${this.path}`}/>
+        </el-dialog>;
     },
     props: {
         path: {
-            type: String,
-            default: ''
+            type: String, default: ''
         }
     },
     data() {
         return {
-            visible: false,
-            sliderValue: 100,
-            videoConf: {
-                duration: 0,//总时长
-                currentTime: 0,//当前已播放时长
-                percent: 0
-            }
+            visible: false
         };
     },
     methods: {
@@ -57,12 +31,9 @@ export default {
             this.visible = true;
         },
         visible(n) {
-            if (!n)
-                this.$refs.video['pause']();
-            else
-                setTimeout(async () => {
-                    this.$refs.video['play']();
-                }, 500);
+            if (!n) this.$refs.video['pause'](); else setTimeout(async () => {
+                this.$refs.video['play']();
+            }, 500);
         }
     }
 };
