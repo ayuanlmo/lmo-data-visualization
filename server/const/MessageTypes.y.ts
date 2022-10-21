8f51253a4c28f5564c89e49258dac78e79227f3f
ayuanlmo <ayuan@ayuanlmo.cn>
2022-10-21 15:12:02 +0800

    feat：完善路径和消息配置

---
+++ server/const/MessageTypes.y.ts
@@ -0,0 +1,5 @@
+export const TASK_PENDING = 'task_pending'; // 任务等待
+export const SHOW_MESSAGE = 'show_message'; // 显示消息
+export const TASK_PROCESSING = 'task_processing'; // 任务处理中
+export const TASK_PRO = 'task_pro'; // 任务生产
+export const TASK_END = 'task_end'; // 任务结束
--- server/const/Path.y.ts
+++ server/const/Path.y.ts
@@ -8,12 +8,12 @@ export default {
         PATH: './static/log/'
     },
     SYNTHESIS: {
-        CURRENT_TEMPLATE: '/static/temp/$t/index.html\n',
-        SERVER: '/static/temp/$t/index.html',
-        OUTPUT: 'static/output/$t.mp4'
+        CURRENT_TEMPLATE: '/static/temp/$y/index.html\n',
+        SERVER: '/static/temp/$y/index.html',
+        OUTPUT: 'static/output/$y.mp4'
     },
     PROCESS_AUDIO: {
-        PATH: 'static/output/$t.mp4',
-        OUTPUT:'static/output/$t264.mp4'
+        PATH: 'static/output/$y.mp4',
+        OUTPUT:'static/output/$y264.mp4'
     }
 };
