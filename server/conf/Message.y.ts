0ddfa437e819e868dd07ab2cbe1c561332b96203
ayuanlmo <ayuan@ayuanlmo.cn>
2022-10-23 01:08:46 +0800

    ref：配置消息

--- server/conf/Message.y.ts
+++ server/conf/Message.y.ts
@@ -1,15 +1,16 @@
 export default {
-    __NO_FFMPEG: '当前服务器缺少必要组件，合成服务将无法正常工作。',
-    __NO_264LIB: '当前服务器缺少[H.264]组件，合成服务可能无法正常工作。',
-    __FILE_NS: '文件$t非受支持的媒体文件，请重新上传',
-    __NO_FILE: '请选择一个文件',
+    __NO_FFMPEG: '当前服务器缺少ffmpeg组件，合成服务将无法正常工作',
+    __NO_264LIB: '当前服务器缺少[H.264]组件，合成服务可能无法正常工作',
+    __FILE_NS: '文件$t非受支持的媒体档案，请重新选择',
+    __NO_FILE: '请选择一个档案',
     __SYNTHESIS_CLOSE: '当前服务器未开启合成服务',
-    __UPLOAD_CLOSE: '当前服务器未开启文件上传',
+    __UPLOAD_CLOSE: '当前服务器不支持档案上传',
     __NO_MEDIA_ID: '请传入Media ID',
     __DEL_MEDIA_ERROR: '删除媒体文件失败：[数据库错误]',
-    __DEL_PROHIBITED: '当前模板禁止删除',
+    __DEL_PROHIBITED: '您无法删除默认模板',
+    __EDIT_DEFAULT_TEMPLATE: '您无法编辑默认模板',
     __DEL_TEMPLATE_ERROR_NT: '未查询到当前模板，请检查ID是否正确',
     __DEL_TEMPLATE_ERROR: '模板删除失败：[数据库错误]',
-    __EDIT_TEMPLATE_ERROR: '编辑删除失败：[数据库错误]',
-    __LIVE_SERVER: '演示服务器不允许进行该操作'
+    __EDIT_TEMPLATE_ERROR: '模板信息编辑失败：[数据库错误]',
+    __LIVE_SERVER: '当前服务器为演示服务器,不允许进行该操作'
 }
