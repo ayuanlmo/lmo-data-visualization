005c53106842d1ee050d97ff107fc83f3111087b
ayuanlmo <ayuan@ayuanlmo.cn>
2022-10-23 02:53:15 +0800

    feat：完善基础方法

--- server/bin/BasicsFuncs.y.ts
+++ server/bin/BasicsFuncs.y.ts
@@ -1,15 +1,40 @@
-// import Conf from "./conf/Conf.y";
 import Message from "../conf/Message.y";
-import {Request, Response} from "express";
-import YingDB from "../lib/sqlite/DataBase.y";
-import {TempLateItemApp} from "../interface/DataBase.y";
-import {CREATE_ERROR_MESSAGE, CREATE_SUCCESS_MESSAGE} from "../utils/Utils.y";
-import {DeleteMedia, MediaType, MediaTypeApp} from "../interface/Media.y";
-import {EditTemplateInfoParams} from "../interface/Template.y";
 import Conf from "../conf/Conf.y";
+import DefaultConf from "../conf/Default.y";
+import FileTypes from "../const/FileTypes.y";
+import MediaTypes from "../const/MediaTypes.Y";
+import YingDB from "../lib/sqlite/DataBase.y";
+import {Request, Response} from "express";
+import {TempLateItemApp} from "../interface/DataBase.y";
+import {DeleteMedia, MediaListType, MediaType, MediaTypeApp} from "../interface/Media.y";
+import {EditTemplateInfoParams} from "../interface/Template.y";
+import {FileType} from "../interface/File.y";
+import {CREATE_ERROR_MESSAGE, CREATE_SUCCESS_MESSAGE, TO_UTF8} from "../utils/Utils.y";
 
-// const Fs = require('fs-extra');
-// const Path = require('path');
+const Fs = require('fs-extra');
+const Path = require('path');
+
+function FilterMediaFile(list: Array<string>, type: string = '__AUDIO') {
+    const List: Array<MediaListType> = [];
+
+    list.filter((i: string) => {
+        return MediaTypes[type].includes(i.split('.')[1]);
+    }).map((i: string) => {
+        const media: Array<string> = i.split('.');
+
+        List.push({
+            name: media[0],
+            type: media[1],
+            path: `/static/uploads/${i}`
+        });
+    });
+
+    return List;
+}
+
+async function DeleteTempFile(path: string): Promise<void> {
+    await Fs.unlink(path);
+}
 
 export async function GetTemplateList(res: Response): Promise<void> {
     const list: Array<object> = [];
@@ -92,7 +117,7 @@ export async function GetMedia(req: Request, res: Response): Promise<void> {
     });
 }
 
-export async function DeleteMedia(req: Request, res: Response): Promise<any> {
+export async function DeleteMedia(req: Request, res: Response): Promise<void> {
     if (Conf.__LIVE_SERVER)
         res.json(CREATE_ERROR_MESSAGE({}, Message.__LIVE_SERVER));
     else {
@@ -106,4 +131,44 @@ export async function DeleteMedia(req: Request, res: Response): Promise<any> {
                 res.json(CREATE_ERROR_MESSAGE({}, Message.__DEL_MEDIA_ERROR))
         });
     }
+}
+
+export async function UpLoadMediaFile(req: Request, res: Response): Promise<any> {
+    if (!DefaultConf.__UPLOAD)
+        return res.json(CREATE_ERROR_MESSAGE({}, Message.__UPLOAD_CLOSE));
+
+    const File: FileType = req['file'];
+
+    if (!File)
+        return res.json(CREATE_ERROR_MESSAGE({}, Message.__NO_FILE));
+
+    File.originalname = TO_UTF8(File.originalname);
+    const OriginPath: string = Path.resolve('server/../static/uploads');
+    const TempFilePath: string = `${OriginPath}/${File.filename}`;
+
+    if (!FileTypes.includes(File.mimetype)) {
+        await DeleteTempFile(TempFilePath);
+        return res.json(CREATE_ERROR_MESSAGE({}, Message.__FILE_NS.replace('$y', `[${File.originalname}]`)))
+    }
+    const Extname: Array<string> = File.originalname.split('.');
+    Extname[1].split(' ').join('');
+    const Fr: any = Fs.createReadStream(File.path);
+    const FileName: string = `${Extname[0]}${new Date().getTime()}.${Extname[1]}`;
+    const Fw = Fs.createWriteStream(`${OriginPath}/${FileName}`);
+    await Fr.pipe(Fw);
+    await DeleteTempFile(TempFilePath);
+    await res.json(CREATE_SUCCESS_MESSAGE({
+        path: `/static/uploads/${FileName}`
+    }));
+}
+
+export async function GetUploadMediaFile(res: Response): Promise<void> {
+    Fs.readdir(Path.resolve('server/../static/uploads'), (err: never, list: Array<any>) => {
+        if (!err)
+            res.json(CREATE_SUCCESS_MESSAGE({
+                audioMedia: FilterMediaFile(list, '__AUDIO'),
+                videoMedia: FilterMediaFile(list, '__VIDEO'),
+                imageMedia: FilterMediaFile(list, '__IMAGE'),
+            }));
+    });
 }
