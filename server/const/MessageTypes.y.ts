26490aac59e0a059b91bce6f5f8c143da5596553
ayuanlmo <ayuan@ayuanlmo.cn>
2022-10-24 04:00:48 +0800

    feat：Message type

--- server/const/MessageTypes.y.ts
+++ server/const/MessageTypes.y.ts
@@ -1,7 +1,8 @@
-export const TASK_PENDING = 'task_pending'; // 任务等待
-export const SHOW_MESSAGE = 'show_message'; // 显示消息
-export const TASK_PROCESSING = 'task_processing'; // 任务处理中
-export const TASK_PRO = 'task_pro'; // 任务生产
-export const TASK_END = 'task_end'; // 任务结束
-export const CREATE_TEMPLATE = 'create_template'; // 创建模板
-export const SUCCESS = 'success'; // 成功
+export const TASK_PENDING: string = 'task_pending'; // 任务等待
+export const SHOW_MESSAGE: string = 'show_message'; // 显示消息
+export const TASK_PROCESSING: string = 'task_processing'; // 任务处理中
+export const TASK_PRO: string = 'task_pro'; // 任务生产
+export const TASK_ERROR: string = 'task_pro_error';
+export const TASK_END: string = 'task_end'; // 任务结束
+export const CREATE_TEMPLATE: string = 'create_template'; // 创建模板
+export const SUCCESS: string = 'success'; // 成功
