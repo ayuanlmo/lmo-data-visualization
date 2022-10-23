783d7eba2e925fe68b926bda9055752d2d79ca3e
ayuanlmo <ayuan@ayuanlmo.cn>
2022-10-24 00:08:20 +0800

    ref：修改前端接口地址

--- src/const/InterfaceUrls.js
+++ src/const/InterfaceUrls.js
@@ -1,7 +1,7 @@
-export const GET_TEMPLATE_DATA = '/api/getTemplate/';
-export const GET_MEDIA = '/api/getMedia/';
-export const UPLOAD_MEDIA = '/api/uploadMedia/';
-export const GET_UPLOAD_MEDIA = '/api/getUploadMedia/';
-export const DEL_MEDIA = '/api/deleteMedia';
-export const DEL_TEMPLATE = '/api/deleteTemplate';
-export const EDIT_TEMPLATE = '/api/editTemplateInfo';
+export const GET_TEMPLATE_DATA = '/api/GetTemplate/';
+export const GET_MEDIA = '/api/GetMedia/';
+export const UPLOAD_MEDIA = '/api/UpLoadMediaFile/';
+export const GET_UPLOAD_MEDIA = '/api/GetUploadMediaFile/';
+export const DEL_MEDIA = '/api/DeleteMedia/';
+export const DEL_TEMPLATE = '/api/DeleteTemplate/';
+export const EDIT_TEMPLATE = '/api/EditTemplateInfo/';
