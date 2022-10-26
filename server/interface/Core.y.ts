a34cac681f22c6b6f824deb2f123d3fef7e58d98
ayuanlmo <ayuan@ayuanlmo.cn>
2022-10-26 15:16:31 +0800

    feat：完善数据类型

--- server/interface/Core.y.ts
+++ server/interface/Core.y.ts
@@ -13,3 +13,34 @@ export interface CopyFileItemType {
 export interface FluentFfmpegErrorTypes {
     message: string;
 }
+
+interface CreateTemplateDataType {
+    customize: {
+        title: string;
+        description: string;
+    }
+}
+
+export interface CreateTaskDataType extends CreateTemplateDataType {
+    name: string;
+    templateConfig: {
+        background: {
+            image: string;
+        };
+        isCustom: number;
+        currentConfig: {};
+    };
+    config: {
+        video: {
+            duration: number;
+            fps: string;
+            clarity: string;
+        };
+        audio: {
+            src: string;
+            volume: string;
+            complete: boolean;
+        };
+    },
+    template: string
+}
