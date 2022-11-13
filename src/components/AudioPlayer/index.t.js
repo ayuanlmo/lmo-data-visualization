3ca9481032e665347da2c5a666a572895837f9b3
ayuanlmo <ayuan@ayuanlmo.cn>
2022-11-13 23:44:02 +0800

    fix：修复LmoAudioPlayer执行play后立即执行pause所导致的异常

--- src/components/AudioPlayer/index.t.js
+++ src/components/AudioPlayer/index.t.js
@@ -2,61 +2,80 @@
  * @class LmoAudioPlayer
  * @constructor
  * @param src {string} Audio Url
+ * @author ayuanlmo
+ * @description 适用于WebApp的简易音频播放器组件
  * **/
 
 export default class LmoAudioPlayer {
     constructor(src = '') {
         this.Audio = null;
-        this.Playing = false;
         this.src = src;
         this.init();
     }
 
+    // 初始化
     init() {
         if (this.Audio === null) {
-            this.Audio = document.createElement('audio');
+            // 创建Audio对象 并加载音频文件
+            this.Audio = new Audio(this.src);
             this.Audio.addEventListener('ended', () => {
                 this.pause();
             });
-            this.setSrc(this.src);
         }
     }
 
-    setVolume(v) {
-        this.Audio.volume = v;
-    }
-
+    // 设置音频文件地址
     setSrc(src) {
         this.Audio.src = src;
         this.src = src;
+        this.load();
     }
 
+    // 重新加载
     load() {
         this.Audio.load();
     }
 
+    /**
+     * @method play
+     * @description 开始播放
+     * @param s {boolean} 从头开始
+     * **/
     play(s = true) {
-        if (s) this.Audio.currentTime = 0;
-        this.Audio.play();
+        this.pause().then(() => {
+            if (s) this.Audio.currentTime = 0;
+            setTimeout(() => {
+                this.Audio.play();
+            });
+        });
     }
 
+    // 暂停
     pause() {
-        if (!this.Audio.paused) {
-            this.Audio.pause();
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                // 异步暂停 防止造成意外的调用
+                if (!this.Audio.paused)
+                    this.Audio.pause();
+                resolve();
+            });
             this.Audio.currentTime = 0;
-        }
+        });
     }
 
+    // 获取音频对象
     ref() {
         return this.Audio;
     }
 
+    // 销毁
     destroy() {
         if (!this.Audio.paused)
             this.Audio.pause();
         this.Audio = null;
     }
 
+    // 继续播放
     continue() {
         this.play(false);
     }
