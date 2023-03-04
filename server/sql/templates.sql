7abb6a8ebc2f512daf2e32b0a867de45f71a099c
ayuanlmo <ayuan@ayuanlmo.cn>
2023-03-05 02:52:00 +0800

    feat：新增模板sql文件

---
+++ server/sql/templates.sql
@@ -0,0 +1,10 @@
+/*
+	模板文件，请先创建 'y_template'表后运行此sql脚本
+*/
+
+
+INSERT INTO `y_template` VALUES ('StackedLine', 'lmo_data_visualization_template_StackedLine', '多折线图', '适用于随时间推移的趋势分析', '/static/DataVisualizationTemplate/StackedLine/index.html', '0');
+INSERT INTO `y_template` VALUES ('SingleHistogram', 'lmo_data_visualization_template_SingleHistogram', '单柱状图', '适用于单条数据对比分析', '/static/DataVisualizationTemplate/SingleHistogram/index.html', '0');
+INSERT INTO `y_template` VALUES ('ChinaMap', 'lmo_data_visualization_template_ChinaMap', '中国地图', '适用于地区之间数据对比分析', '/static/DataVisualizationTemplate/ChinaMap/index.html', '0');
+INSERT INTO `y_template` VALUES ('Histogram', 'lmo_data_visualization_template_Histogram', '多柱状图', '适用于多组数据对比分析', '/static/DataVisualizationTemplate/Histogram/index.html', '0');
+INSERT INTO `y_template` VALUES ('DynamicSortingDiagram', 'lmo_data_visualization_template_DynamicSortingDiagram', '动态排序图', '适用于多组数据动态分析', '/static/DataVisualizationTemplate/DynamicSortingDiagram/index.html', '0');
