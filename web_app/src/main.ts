edbf68b54451d55216f9e2e2cb3f5e623f2a0b4c
lxue <xue283866@gmail.com>
2023-08-31 20:08:34 +0800

    refactor: Refactor Vue Single File Component to TSX Format as a Sample Implementation for the Project
    1. Refactored the HelloWorld.vue single file component to HelloWorld.tsx.
    2. Directly imported the new HelloWorld.tsx in main.ts.
    
    This commit serves as a sample implementation for the project, aiming to modernize the codebase.

--- web_app/src/main.ts
+++ web_app/src/main.ts
@@ -1,5 +1,5 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import {createApp} from 'vue';
+import App from './App.tsx';  // 注意这里改为了TSX文件
+import './style.css';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
