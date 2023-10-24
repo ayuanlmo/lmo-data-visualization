85ca724023915929be2a5e98796734c6d815938b
ayuanlmo <ayuan@ayuanlmo.cn>
2023-10-24 18:33:29 +0800

    add：AudioPlayer class

---
+++ web_app/src/lib/AudioPlayer/index.ts
@@ -0,0 +1,114 @@
+export interface AudioPlayerInterface {
+    isDestroy: boolean;
+    canplay: boolean;
+    load: () => void;
+    setSrc: (src: string) => Promise<void>;
+    play: (s?: boolean) => void;
+    setVolume: (v: number) => void;
+    destroy: () => void;
+    continue: (src: string) => Promise<void>;
+    pause: (src: string) => Promise<void>;
+    getRef: () => HTMLAudioElement;
+    addEventListener: <T extends keyof HTMLMediaElementEventMap>(event: T, listener: (...args: Array<HTMLMediaElementEventMap[T]>) => any) => void;
+    removeEventListener: <T extends keyof HTMLMediaElementEventMap>(event: T, listener: (...args: Array<HTMLMediaElementEventMap[T]>) => any) => void;
+}
+
+class AudioPlayer implements AudioPlayerInterface {
+    private audioObject: HTMLAudioElement | null;
+    private url: string;
+    public isDestroy: boolean;
+    public canplay: boolean;
+
+    constructor(url: string) {
+        this.url = url;
+        this.audioObject = null;
+        this.isDestroy = false;
+        this.canplay = false;
+        this.init();
+    }
+
+    private init(): void {
+        if (this.audioObject === null && (!this.isDestroy)) {
+            if (!('Audio' in window)) {
+                throw '(AudioPlayer) Error: Your browser does not support audio playback.';
+            }
+            this.audioObject = new Audio(this.url);
+            this.audioObject.addEventListener('ended', (): void => {
+                this.audioObject?.pause();
+            });
+            this.audioObject.addEventListener('canplaythrough', (): void => {
+                this.canplay = true;
+            });
+        }
+    }
+
+    public load(): void {
+        this.audioObject?.paused && this.audioObject.pause();
+        this.audioObject?.load();
+    }
+
+    public async setSrc(url: string): Promise<void> {
+        this.url = url;
+        await this.pause();
+        // @ts-ignore
+        this.audioObject?.src = this.url;
+        this.load();
+    }
+
+    public play(s: boolean = false): void {
+        this.pause().then(async (): Promise<void> => {
+            if (s) {
+                // @ts-ignore
+                this.audioObject?.currentTime = 0;
+                setTimeout(async (): Promise<void> => {
+                    await this.audioObject?.play();
+                });
+            } else
+                await this.continue();
+        });
+    }
+
+    public setVolume(v: number): void {
+        // @ts-ignore
+        this.audioObject?.volume = v;
+    }
+
+    public destroy(): void {
+        if (!this.audioObject?.paused)
+            this.pause();
+        this.audioObject = null;
+        this.isDestroy = true;
+    }
+
+    public async continue(): Promise<void> {
+        await this.audioObject?.play();
+    }
+
+    public getRef(): HTMLAudioElement {
+        return this.audioObject as HTMLAudioElement;
+    }
+
+    public addEventListener<T extends keyof HTMLMediaElementEventMap>(event: T, listener: (...args: Array<HTMLMediaElementEventMap[T]>) => any): void {
+        if (event)
+            this.audioObject?.addEventListener(event, listener);
+    }
+
+    // 移除事件侦听器
+    public removeEventListener<T extends keyof HTMLMediaElementEventMap>(event: T, listener: (...args: Array<HTMLMediaElementEventMap[T]>) => any): void {
+        if (event)
+            this.audioObject?.removeEventListener(event, listener);
+    }
+
+    public pause(): Promise<void> {
+        return new Promise((resolve): void => {
+            setTimeout((): void => {
+                if (!this.audioObject?.paused) {
+                    this.audioObject?.pause();
+                }
+                resolve();
+            })
+        });
+    }
+}
+
+export default AudioPlayer;
