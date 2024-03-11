248412480263573af5efbc70bea817bd61d7bbcf
ayuanlmo <ayuanlmo@outlook.com>
2024-03-12 01:15:47 +0800

    feat：自动调整预览器比例

--- web_app/src/components/Preview.tsx
+++ web_app/src/components/Preview.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
+import {useEventListener} from "../bin/Hooks";
 
 function TemplatePreview(): React.JSX.Element {
+    const templatePreviewRef: React.RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+    const calculateAspectRatio = (width: number): [number, number] => {
+        return [width, width / 16 * 9];
+    };
+    const initHeight = (): void => {
+        const [, height] = calculateAspectRatio(templatePreviewRef.current?.offsetWidth || 0);
+
+        templatePreviewRef.current && (templatePreviewRef.current.style.height = `${height}px`);
+    };
+
+    useEffect((): void => {
+        initHeight();
+    }, [templatePreviewRef]);
+
+    useEventListener('resize', (): void => {
+        initHeight();
+    });
     return (
-        <div className={'template-preview'}></div>
+        <div ref={templatePreviewRef} className={'template-preview'}></div>
     );
 }
 
--- web_app/src/pages/AppDesign.tsx
+++ web_app/src/pages/AppDesign.tsx
@@ -5,6 +5,7 @@ import Grid from "@hi-ui/grid";
 import TemplatePreview from "../components/Preview";
 import ProgressBar from "../components/ProgressBar";
 import DesignConfigs from "../components/DesignConfigs";
+import YExtendTemplate from "../components/YExtendTemplate";
 
 function AppDesign(): React.JSX.Element {
     const {Row, Col} = Grid;
@@ -14,18 +15,29 @@ function AppDesign(): React.JSX.Element {
             <DesignHeader/>
 
             <div className={'app_position_relative'} style={{
-                width: "calc(100% - (2.5rem * 2))",
-                margin: "1.5rem auto"
+                // width: "calc(100% - (2.5rem * 2))",
+                // margin: "1.5rem auto",
+                padding: "1.5rem",
+                height: "calc(100vh - (4.125rem + 1.5rem))"
             }}>
-                <Row justify={'space-between'}>
-                    <Col span={{lg: 16, xl: 16, md: 24, sm: 24, xs: 24}}>
-                        <TemplatePreview/>
-                        <ProgressBar/>
-                    </Col>
-                    <Col span={{lg: 7, xl: 7.5, md: 24, sm: 24, xs: 24}}>
-                        <DesignConfigs/>
-                    </Col>
-                </Row>
+                <div style={{
+                    padding: "1.5rem"
+                }}>
+                    <Row style={{
+                        height: '98%'
+                    }} justify={'space-between'}>
+                        <YExtendTemplate show={true}>
+                            <Col span={{lg: 16, xl: 18, md: 24, sm: 24, xs: 24}}>
+                                <TemplatePreview/>
+                                <ProgressBar/>
+                            </Col>
+                            <Col span={{lg: 7, xl: 5.5, md: 24, sm: 24, xs: 24}}>
+                                <DesignConfigs/>
+                            </Col>
+                        </YExtendTemplate>
+                    </Row>
+                </div>
+
             </div>
         </div>
     );
