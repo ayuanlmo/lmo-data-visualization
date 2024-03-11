1278aa24f34f2496e1a0e06daca1c0b3670e8b33
ayuanlmo <ayuan@ayuanlmo.cn>
2024-03-11 19:07:59 +0800

    feat：message提示框

--- web_app/src/lib/Notification.ts
+++ web_app/src/lib/Notification.ts
@@ -1,4 +1,5 @@
 import {notification} from "@hi-ui/hiui";
+import message from "@hi-ui/message";
 
 class Notification {
     public static openNotification(title: string = '通知', content: string, type: 'info' | 'success' | 'error' | 'warning' = 'error'): void {
@@ -7,6 +8,14 @@ class Notification {
             title: title,
             type: type,
             content: content
+        })
+    }
+
+    public static message(title: string, type: 'info' | 'success' | 'error' | 'warning'): void {
+        message.closeAll();
+        message.open({
+            title: title,
+            type: type
         })
     }
 }
