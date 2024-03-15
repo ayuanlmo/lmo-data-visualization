b519810332b4bea4ddeed1ade8ea1dddbc466ea8
ayuanlmo <ayuan@ayuanlmo.cn>
2024-03-15 18:04:51 +0800

    feat：模板消息接口

---
+++ web_app/src/types/TemplateMessage.ts
@@ -0,0 +1,34 @@
+export type TTemplateTextConfigAlignType = 'left' | 'center' | 'right';
+
+export type TTemplateMessageType =
+    'TEMPLATE_RENDER'
+    | 'TEMPLATE_RENDER_FINISH'
+    | 'TEMPLATE_RENDER_ERROR'
+    | 'TEMPLATE_FULL_CONFIG'
+    |'TEMPLATE_SELECT_TEXT_ELEMENT'
+
+export type TDesignAppMessageType =
+    'SET_DATA'
+    | 'SET_TEXT_CONFIG'
+    | 'SET_COLOR_MODE'
+    | 'SET_COLOR_CONFIG'
+    | 'SET_THEME_COLOR'
+    | 'SET_BACKGROUND_IMAGE'
+    | 'SET_TITLE_ANIMATE'
+    | 'SET_DURATION'
+    | 'GET_FULL_CONFIG'
+    | 'PREVIEW_MODE'
+    | 'PLAY_MODE'
+
+export interface ITemplateSelectTextElement {
+    color: string;
+    value: string;
+    display: boolean;
+    fontSize: number;
+    align: TTemplateTextConfigAlignType;
+    width: number;
+    height: number;
+    x: number;
+    y: number;
+}
+
