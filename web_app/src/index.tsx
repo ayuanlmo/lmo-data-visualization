39440d68e227b8034841dce29b3273e4fe11194c
ayuanlmo <ayuanlmo@outlook.com>
2024-03-26 21:28:54 +0800

    fix：修复dark-mode下hi-ui样式不正确

--- web_app/src/index.tsx
+++ web_app/src/index.tsx
@@ -7,14 +7,15 @@ import Store from "./lib/Store/";
 import "./style/Global.css";
 import "./style/index.scss";
 import "./style/animate.min.css";
-import "./style/hi.css";
+import "./style/hi.scss";
 
 ((): void => {
     const AppHome: React.LazyExoticComponent<() => React.JSX.Element> = lazy(() => import("./pages/AppHome"));
     const AppDesign: React.LazyExoticComponent<() => React.JSX.Element> = lazy(() => import("./pages/AppDesign"));
-    const __RootApp: Root = createRoot(
-        document.getElementById('__lmo_app') as HTMLDivElement
-    );
+    const RootDom: HTMLDivElement = document.getElementById('__lmo_app') as HTMLDivElement;
+    const __RootApp: Root = createRoot(RootDom);
+
+    document.body.classList.add('dark-mode');
 
     __RootApp.render(
         <Provider store={Store}>
