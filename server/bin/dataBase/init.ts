1fe18bc8de9f18f701912cc9aece2bec3f0abe02
ayuanlmo <ayuan@ayuanlmo.cn>
2024-04-12 14:38:48 +0800

    feat：模板初始化成功后保留initialized文件

--- server/bin/dataBase/init.ts
+++ server/bin/dataBase/init.ts
@@ -33,8 +33,16 @@ const initTemplate = () => {
     templateFiles.map((i: string): void => {
         const templateItem: string[] = fs.readdirSync(path.resolve(templatePath + `/${i}`));
 
-        // 检查当前模板是否存在必要的依赖
-        if (checkArrayIncludes(templateItem, [
+        if (checkArrayIncludes(templateItem, ['.initialized.t.bin'])) {
+            const initData: string = fs.readFileSync(templatePath + `/${i}/.initialized.t.bin`).toString();
+
+            try {
+                templateData.push(JSON.parse(initData));
+            } catch (e) {
+                Cli.warn('模板初始化文件加载失败', e);
+            }
+            // 检查当前模板是否存在必要的依赖
+        } else if (checkArrayIncludes(templateItem, [
             'conf.js',
             'data.json',
             'cover.png',
@@ -50,17 +58,18 @@ const initTemplate = () => {
                     type: number;
                 } = JSON.parse(fs.readFileSync(path.resolve(templatePath + `/${i}/config.json`), 'utf-8'));
 
-                const templateItem = {
+                const item = {
                     id: require('uuid').v4(),
                     ...templateConfig,
-                    cover: `${templateRoot}/cover.png`,
-                    path: `${templateRoot}/index.html`,
+                    cover: `${templateRoot.replace('/public', '')}/cover.png`,
+                    path: `${templateRoot.replace('/public', '')}/index.html`,
                     createTime: new Date().getTime()
                 };
 
-                templateData.push(templateItem);
-                // 打一个初始化成功的标记，后续重新加载模板时可以忽略当前模板
-                fs.writeFileSync(path.resolve(templatePath + `/${i}/.initialized`), JSON.stringify(templateItem));
+                templateData.push(item);
+                // 打一个初始化成功的标记文件，后续重新加载模板时可以忽略当前模板
+                fs.writeFileSync(path.resolve(templatePath + `/${i}/.initialized.t.bin`), Buffer.from(JSON.stringify(item)));
+
             } catch (e) {
                 Cli.warn(`模板初始化失败，请检查${templatePath + `/${i}/config.json`}是否存在或正确配置`, e);
             }
