72937d233240ae85745283bacb0e6aeec0544c26
ayuanlmo <ayuanlmo@outlook.com>
2024-07-15 18:05:49 +0800

    feat：服务器支持aac格式音频上传

--- server/const/UpLoadFileTypes.ts
+++ server/const/UpLoadFileTypes.ts
@@ -1,3 +1,3 @@
-const UpLoadFileTypes = ['image/jpeg', 'image/png', 'image/svg+xml', 'audio/mp4', 'audio/mpeg', 'video/mp4', 'audio/x-m4a', 'audio/flac'];
+const UpLoadFileTypes = ['image/jpeg', 'image/png', 'image/svg+xml', 'audio/mp4', 'audio/mpeg', 'video/mp4', 'audio/x-m4a', 'audio/flac', 'audio/aac'];
 
 export default UpLoadFileTypes;
