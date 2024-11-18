2f0b975b1ba2347095b37a10263fb3c842962998
ayuanlmo <ayuanlmo@outlook.com>
2024-11-19 03:50:40 +0800

    feat：增加欢迎页

--- web_app/src/pages/AppHome.tsx
+++ web_app/src/pages/AppHome.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, {useEffect} from "react";
 import TemplateList from "../components/TemplateList";
 import Header from "../components/Header";
+import Storage from "../lib/Storage";
+import {NavigateFunction, useNavigate} from "react-router-dom";
 
 const AppHome = (): React.JSX.Element => {
+    const navigate: NavigateFunction = useNavigate();
+
+    useEffect((): void => {
+        if (!location.pathname.includes('welcome') && Storage.get('open_welcome_page') === null)
+            navigate('/welcome');
+    }, []);
+
     return (
         <div className={'data-visualization app_none_user_select'}>
             <Header.Home/>
