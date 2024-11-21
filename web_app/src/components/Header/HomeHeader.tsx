d05b34324458b7597fc672cd9efa492f62780346
ayuanlmo <ayuanlmo@outlook.com>
2024-11-21 18:00:32 +0800

    pref：调整首页header

--- web_app/src/components/Header/HomeHeader.tsx
+++ web_app/src/components/Header/HomeHeader.tsx
@@ -2,7 +2,7 @@ import React, {useRef} from "react";
 import {useTranslation} from "react-i18next";
 import AppConfig from "../../config/AppConfig";
 import GlobalComponent from "../GlobalComponent";
-import {Button} from "@hi-ui/hiui";
+import {Button, Grid} from "@hi-ui/hiui";
 import SelectFile, {ISelectFileRef} from "../SelectFile";
 import SwitchLang from "../GlobalComponent/components/SwitchLang";
 import YExtendTemplate from "../YExtendTemplate";
@@ -17,47 +17,63 @@ const HomeHeader = (): React.JSX.Element => {
 
     return (
         <div className={'data-visualization-header'}>
-            <div
-                className={'data-visualization-header-app-name app_cursor_pointer header-item'}
-                onClick={(): void => {
-                    aboutRef.current?.open?.();
-                }}
-            >
-                {AppConfig.appName}
-            </div>
-            <About ref={aboutRef}/>
-            <div
-                className={'data-visualization-header-option header-item'}
-                style={{
-                    marginTop: '1.3rem'
-                }}
-            >
-                <GlobalComponent.SwitchTheme style={{
-                    marginTop: '0'
-                }}/>
-                <YExtendTemplate show={false}>
-                    <Button size={'sm'} type="secondary">{t('log')}</Button>
-                </YExtendTemplate>
-                <Button
-                    size={'sm'}
-                    type="primary"
-                    onClick={(): void => {
-                        resourcesRef.current?.open();
-                    }}
-                >
-                    {t('resLib')}
-                </Button>
-                <Button
-                    size={'sm'}
-                    type="default"
-                    onClick={(): void => {
-                        selectFileRef.current?.open();
-                    }}
-                >{t('materialLib')}</Button>
-                <SwitchLang/>
-                <Resources ref={resourcesRef}/>
-                <SelectFile isUse={false} ref={selectFileRef}/>
-            </div>
+            <Grid.Row style={{
+                width: '100%'
+            }} justify={'space-between'}>
+                <Grid.Col span={
+                    {lg: 8, xl: 8, md: 12, sm: 12, xs: 12}
+                }>
+                    <React.Fragment>
+                        <div className={'data-visualization-header-app-name'}>
+                            <span
+                                className={'app_cursor_pointer'}
+                                onClick={(): void => {
+                                    aboutRef.current?.open?.();
+                                }}
+                            >
+                                {AppConfig.appName}
+                            </span>
+                        </div>
+                        <About ref={aboutRef}/>
+                    </React.Fragment>
+                </Grid.Col>
+                <Grid.Col span={
+                    {lg: 12, xl: 8, md: 12, sm: 12, xs: 12}
+                }>
+                    <div
+                        className={'data-visualization-header-option'}
+                        style={{
+                            marginTop: '1.3rem'
+                        }}
+                    >
+                        <GlobalComponent.SwitchTheme style={{
+                            marginTop: '0'
+                        }}/>
+                        <YExtendTemplate show={false}>
+                            <Button size={'sm'} type="secondary">{t('log')}</Button>
+                        </YExtendTemplate>
+                        <Button
+                            size={'sm'}
+                            type="primary"
+                            onClick={(): void => {
+                                resourcesRef.current?.open();
+                            }}
+                        >
+                            {t('resLib')}
+                        </Button>
+                        <Button
+                            size={'sm'}
+                            type="default"
+                            onClick={(): void => {
+                                selectFileRef.current?.open();
+                            }}
+                        >{t('materialLib')}</Button>
+                        <SwitchLang/>
+                        <Resources ref={resourcesRef}/>
+                        <SelectFile isUse={false} ref={selectFileRef}/>
+                    </div>
+                </Grid.Col>
+            </Grid.Row>
         </div>
     );
 };
