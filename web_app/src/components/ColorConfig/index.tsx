82a09fb97ad159efc12b368a2c3d80fa92a923a1
ayuanlmo <ayuanlmo@outlook.com>
2025-07-14 12:48:49 +0800

    feat：默认隐藏不受支持的主题色

--- web_app/src/components/ColorConfig/index.tsx
+++ web_app/src/components/ColorConfig/index.tsx
@@ -67,7 +67,7 @@ function ColorConfig(): React.JSX.Element {
             {title: t('themeColor'), id: 'Theme', disabled: !configTheme.configs.includes('Theme')},
             {title: t('gradientColor'), id: 'Gradient', disabled: !configTheme.configs.includes('Gradient')},
             {title: t('singleColor'), id: 'Single', disabled: !configTheme.configs.includes('Single')}
-        ]);
+        ].filter((i) => !i.disabled));
 
         setThemeType(configTheme.type);
     }, [configTheme]);
