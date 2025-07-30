a025e8d50fac617909456124a50d35dd36a2586c
ayuanlmo <ayuanlmo@outlook.com>
2025-07-30 08:37:55 +0800

    feat：非开发环境使用sa用户时，程序抛出异常

--- server/bin/dataBase/index.ts
+++ server/bin/dataBase/index.ts
@@ -63,6 +63,8 @@ const dbHost: string = process.env.DATA_BASE_HOST ?? '';
 const DANGEROUS_SQL_SERVER_ACCOUNT: string = 'sa' as const;
 
 if (dbType === 'mssql' && dbUserName?.trim().toLowerCase() === DANGEROUS_SQL_SERVER_ACCOUNT && !AppConfig.__DEV_SERVER)
+    throw new Error('Please do not use "sa" as your database username');
+
 const DB: Sequelize = dbType === 'mssql' ?
     new Sequelize(dbName, dbUserName, dbPassWord, {
         host: dbHost,
